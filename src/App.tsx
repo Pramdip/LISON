4c4e078be73a7980dc5668668ea4abd9abca6638
Pramdip <np03cs4a220289@heraldcollege.edu.np>
2025-09-06 01:13:12 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import './pages/dashboard'
+import Dashboard from './pages/dashboard';
+
+
+function App() {
+  return <>
+  <Dashboard/>
+  </>;
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+ // default 400 weight
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
